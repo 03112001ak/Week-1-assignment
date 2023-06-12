8cbd95443febcd7d75fe6bc0cc3891f2dcaa53e8
03112001ak <70797860+03112001ak@users.noreply.github.com>
2023-06-12 19:20:01 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  var Anagram=false;
+  if(str1.length===str.length){
+    for(let i=0;i<str1.length;i++){
+      for(let j=0;j<str2.length;j++){
+        if(str1[i]===str2[j])
+          continue;
+        else{
+          return Anagram;
+        }
+        Anagram=true;
+      }
+    }
+  }
 }
 
 module.exports = isAnagram;
